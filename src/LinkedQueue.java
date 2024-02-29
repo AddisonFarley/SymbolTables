89e19ec040aa230c3ca6b921b8c60d25c6785d21
AddisonFarley <93640684+AddisonFarley@users.noreply.github.com>
2024-02-29 06:40:16 -0800

    update files

--- src/LinkedQueue.java
+++ src/LinkedQueue.java
@@ -1,121 +1,114 @@
+import java.util.ConcurrentModificationException;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 /**
- * Implements the FIFO Queue (linked-list) data structure.
+ * LinkedQueue is a linked list implementation of the
+ * (FIFO) queue interface. Implementation details follow
+ * the Algorithms textbook, pp. 150-151.
+ * @param <E> type of the items/elements in the queue
  *
- * @author Addison Farley
+ * @author Kendrick Hang
  * @version 1.0
  */
 public class LinkedQueue<E> implements Queue<E>
 {
-    //fields
     private Node head;
     private Node tail;
     private int size;
+    private int numModifications;
 
-    //class to create nodes for linkedlist
     private class Node
     {
-        E data;
+        E item;
         Node next;
+
+        public Node(E item, Node next)
+        {
+            this.item = item;
+            this.next = next;
+        }
     }
 
-    /**
-     * Constructor for LinkedQueue.
-     */
     public LinkedQueue()
     {
-        this.head = null;
-        this.tail = null;
-        this.size = 0;
+        head = null;
+        tail = null;
+        size = 0;
+        numModifications = 0;
     }
 
     /**
-     * Add an item to the end of queue.
-     * Time complexity: O(1)
-     * Time is constant because all operations are singular.
+     * Add an item to the queue.
      *
      * @param item the item to be added
+     * @throws IllegalArgumentException if item is null
      */
     @Override
     public void enqueue(E item)
     {
-        //save head to new node
-        Node oldHeaD = head;
-
-        //recreate head as a new node
-        head = new Node();
-
-        //save item to new head's data
-        head.data = item;
-
-        if(isEmpty())
+        if (item == null)
         {
-            //assign head to head if queue is empty
-            tail = head;
+            throw new IllegalArgumentException("item is null");
+        }
+
+        Node oldTail = tail;
+        tail = new Node(item, null);
+
+        if (isEmpty())
+        {
+            head = tail;
         }
         else
         {
-            //reference oldHead to head
-            oldHeaD.next = head;
+            oldTail.next = tail;
         }
 
-        //increment size
         size++;
+        numModifications++;
     }
 
     /**
-     * Remove an item from the beginning of queue.
-     * Time complexity: O(1)
-     * Time is constant because either an exception is thrown or singular operations are executed.
+     * Remove an item from the queue.
      *
      * @return the item that was removed
+     * @throws NoSuchElementException if the queue is empty
      */
     @Override
     public E dequeue()
     {
-        //throw exception if queue is empty, cannot dequeue an empty queue
-        if(isEmpty())
+        if (isEmpty())
         {
-            throw new IllegalStateException("Stack is empty, cannot pop()");
+            throw new NoSuchElementException();
         }
 
-        //save data from node to be removed
-        E item = tail.data;
+        E item = head.item;
+        head = head.next;
 
-        //remove tail
-        tail = tail.next;
-
-        if(isEmpty())
+        if (isEmpty())
         {
-            //set tail to null if queue is empty
-            head = null;
+            tail = null;
         }
 
-        //decrement size
         size--;
+        numModifications++;
 
-        //return item
         return item;
     }
 
     /**
      * Checks to see if the queue is empty.
-     * Time complexity: O(1)
-     * Time is constant due to a single comparison operation.
      *
      * @return true if the queue is empty, false otherwise
      */
     @Override
     public boolean isEmpty()
     {
-        return size == 0;
+        return head == null;
     }
 
     /**
      * Returns a count of the number of items in the queue.
-     * Time complexity: O(1)
-     * Time is constant due to a single comparison operation.
      *
      * @return the number of items in the queue
      */
@@ -133,31 +126,66 @@ public class LinkedQueue<E> implements Queue<E>
     @Override
     public Iterator<E> iterator()
     {
-        return new ListIterator();
+        return new LinkedListIterator();
     }
 
-    private class ListIterator implements Iterator<E>
+    private class LinkedListIterator implements Iterator<E>
     {
-        private Node curr = tail;
+        private Node current;
+        private final int numModificationsAtCreation = numModifications;
 
+        /**
+         * Initializes the iterator by setting the current
+         * position to be the same as the head (front) of
+         * the list.
+         */
+        public LinkedListIterator()
+        {
+            current = head;
+        }
+
+        /**
+         * Returns {@code true} if the iteration has more elements.
+         * (In other words, returns {@code true} if {@link #next} would
+         * return an element rather than throwing an exception.)
+         *
+         * @return {@code true} if the iteration has more elements
+         * @throws ConcurrentModificationException if queue has been modified since this iterator was created
+         */
         @Override
         public boolean hasNext()
         {
-            return !isEmpty();
+            if (numModifications != numModificationsAtCreation)
+            {
+                throw new ConcurrentModificationException("queue has changed!");
+            }
+
+            return current != null;
         }
 
+        /**
+         * Returns the next element in the iteration.
+         *
+         * @return the next element in the iteration
+         * @throws NoSuchElementException if the iteration has no more elements
+         * @throws ConcurrentModificationException if the queue has been modified since this iterator was created
+         */
         @Override
         public E next()
         {
-            E item = tail.data;
-            curr = curr.next;
-            return item;
-        }
+            if (numModifications != numModificationsAtCreation)
+            {
+                throw new ConcurrentModificationException("queue has changed!");
+            }
 
-        @Override
-        public void remove()
-        {
-            //do nothing
+            if (current == null)
+            {
+                throw new NoSuchElementException("current is null!");
+            }
+
+            E item = current.item;  // save current.item so we can return it
+            current = current.next; // advance current to next node in list
+            return item;
         }
     }
 }
